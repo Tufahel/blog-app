98db2121a17ef8520bef6c5e432caf9fdc34af4b
Tufahel <tufahel96@gmail.com>
2022-06-10 20:38:42 +0600

    linters fixed

--- spec/request/users_controller_spec.rb
+++ spec/request/users_controller_spec.rb
@@ -1,35 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-    describe 'GET #index' do
-        before(:example) {get('/users')}
+  describe 'GET #index' do
+    before(:example) { get('/users') }
 
-        it 'returns http success' do
-            expect(response).to have_http_status(:ok)
-        end
-
-        it 'renders index' do
-            expect(response).to render_template(:index)
-        end
-
-        it 'includes the correct text' do
-            expect(response.body).to include 'List of users.'
-        end
+    it 'returns http success' do
+      expect(response).to have_http_status(:ok)
     end
 
-    describe 'GET #show' do
-        before(:example) {get('/users/2')}
-
-        it 'returns http success' do
-            expect(response).to have_http_status(:ok)
-        end
-
-        it 'renders show' do
-            expect(response).to render_template(:show)
-        end
-
-        it 'includes the correct text' do
-            expect(response.body).to include('Show details of an user.')
-        end
+    it 'renders index' do
+      expect(response).to render_template(:index)
     end
+
+    it 'includes the correct text' do
+      expect(response.body).to include 'List of users.'
+    end
+  end
+
+  describe 'GET #show' do
+    before(:example) { get('/users/2') }
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders show' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct text' do
+      expect(response.body).to include('Show details of an user.')
+    end
+  end
 end
