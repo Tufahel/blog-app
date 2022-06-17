d3d48a15f7e1faa3eb672cacf5507e428f43e70f
Tufahel <tufahel96@gmail.com>
2022-06-18 05:28:10 +0600

    add new likes

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,16 @@
+class LikesController < ApplicationController
+def new
+    @like = Like.new
+end
+
+def create
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.new(user_id: current_user.id, post_id: @post)
+
+    if @like.save
+        redirect_to user_post_path(user_id: @post.user_id, id: @post.id)
+    else
+        render :new, alert: 'An error occurred'
+    end
+end
+end
