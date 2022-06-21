c358164b603b0244c22846f864fbe473de802574
Tufahel <tufahel96@gmail.com>
2022-06-21 20:31:36 +0600

    linters fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,15 +8,15 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment.user_id = current_user.id
     @comment.post_id = @post.id
-    
-    if @comment.text.size > 0
-      if @comment.save 
+
+    if @comment.text.size.positive?
+      if @comment.save
         redirect_to user_post_path(user_id: @post.user_id, id: @post.id), notice: 'Your comment is addedd'
       else
         render :new, alert: 'An error occurred while creating comment'
       end
     else
-      redirect_to user_post_comments_path(), alert: 'You forget to add any comment.'
+      redirect_to user_post_comments_path, alert: 'You forget to add any comment.'
     end
   end
 
