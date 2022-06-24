36fb16ee4e56d8e712f57260900c736bbca154a5
Tufahel <tufahel96@gmail.com>
2022-06-25 01:00:27 +0600

    setup cancancan and ability.rb

---
+++ app/models/ability.rb
@@ -0,0 +1,19 @@
+class Ability
+    include CanCan::Ability
+  
+    def initialize(user)
+  
+      user ||= User.new
+      can :read, Post
+      can :read, Comment
+      return unless user.present?
+  
+      can :delete, Post, user: user
+      can :manage, Comment, user: user
+      can :create, Comment
+      can :create, Like
+      return unless user.role == 'admin'
+  
+      can :manage, :all
+    end
+  end
